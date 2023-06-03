bf68f1b44d9d6bdbefbb4cf724aef08b2dae7225
glueink <jegor.salajev@gmail.com>
2023-06-04 02:29:16 +0300

    save query params between routes

--- src/app/providers/withRouter.ts
+++ src/app/providers/withRouter.ts
@@ -1,9 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, type RouteLocationNormalized } from 'vue-router';
 import { routes } from '@/pages';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
+});
+
+function hasQueryParams(route: RouteLocationNormalized) {
+  return !!Object.keys(route.query).length;
+}
+
+router.beforeEach((to, from, next) => {
+  if (!hasQueryParams(to) && hasQueryParams(from)) {
+    next({
+      name: to.name!,
+      query: from.query
+    });
+  } else {
+    next();
+  }
 });
 
 export default router;
